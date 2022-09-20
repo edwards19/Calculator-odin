69806e221e0336925468a554a1c32326b5bc83aa
Eduard <eduard.cojocaru26@gmail.com>
2022-09-20 13:28:53 +0300

    Create functions to show numbers on the display

--- script.js
+++ script.js
@@ -6,8 +6,40 @@ const equals = document.querySelector('.equals');
 const numbers = document.querySelectorAll('.number');
 const operators = document.querySelectorAll('.operator');
 
-const previous = document.querySelector('.previous');
-const current = document.querySelector('.current');
+const previousDisplay = document.querySelector('.previous');
+const currentDisplay = document.querySelector('.current');
+
+let previousValue = '';
+let currentValue = '';
+let lastOperator = '';
+
+
+numbers.forEach((number) => {
+  number.addEventListener('click', (e) => {
+    handleNumber(e.target.textContent);
+    currentDisplay.textContent = currentValue;
+  })
+})
+
+operators.forEach((op) => {
+  op.addEventListener('click', (e) => {
+    handleOperator(e.target.textContent);
+    previousDisplay.textContent = `${previousValue} ${lastOperator}`;
+    currentDisplay.textContent = '0'; // <--previousValue
+  })
+})
+
+function handleNumber(num){
+  if (currentValue.length <= 6){
+    currentValue += num;
+  }
+}
+
+function handleOperator(op){
+  lastOperator = op;
+  previousValue = currentValue;
+  currentValue = '';
+}
 
 function add(a, b){
   return a + b
